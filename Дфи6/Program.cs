d8d7ce2ac35a7d37a636065b8a39a4b39622dbfb
Aleksey Kushnarev <scanavik2005@gmail.com>
2023-12-06 00:35:55 +0500

    Добавьте файлы проекта.

---
+++ Дфи6/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo key;
+            PrintHelpMessage();
+            do
+            {
+                key = Console.ReadKey();
+                string inf = key.Key.ToString();
+                Console.WriteLine();
+                switch (inf)
+                {
+                    case "D0":
+                        PrintHelpMessage();
+                        break;
+                    case "D1":
+                        //Console.WriteLine("1");
+                        WorkWithArrays();
+                        break;
+                    case "D2":
+                        Console.WriteLine("2");
+                        //WorkWithStrings();
+                        break;
+
+                    case "Escape":
+                        break;
+
+                    default:
+                        Console.WriteLine("Такой команды не сущействует. Для вывода списка команд введите 0");
+                        break;
+                }
+            } while (key.Key != ConsoleKey.Escape);
+
+
+        }
+
+        public static void PrintHelpMessage()
+        {
+            Console.WriteLine("0 - Вывод списка команд\n" +
+                              "1 - Работа с массивами\n" +
+                              "2 - Работа со строками\n" +
+                              "Esc - Выход");
+        }
+
+
+        public static void WorkWithArrays()
+        {
+            ArrayFuncs arrayFuncs = new ArrayFuncs();
+            char[,] arayChars;
+            ConsoleKeyInfo key;
+            PrintHelpMessageForArray();
+            do
+            {
+                key = Console.ReadKey();
+                string inf = key.Key.ToString();
+                Console.WriteLine();
+                switch (inf)
+                {
+                    case "D0":
+                        PrintHelpMessageForArray();
+                        break;
+                    case "D1":
+                        
+                        Console.WriteLine("1 - Заполнить массив вручную\n" +
+                                          "2 - Заполнить массив случайными числами");
+                        string methodCreating = Console.ReadLine();
+                        switch (methodCreating)
+                        {
+                            case "1":
+                                bool rnd = false;
+                                arayChars = ArrayFuncs.CreateArray(rnd);
+                                Console.WriteLine(ArrayFuncs.PrintArray(arayChars));
+                                break;
+
+                            case "2":
+                                arayChars = ArrayFuncs.CreateArray();
+                                Console.WriteLine(ArrayFuncs.PrintArray(arayChars));
+                                break;
+
+                            default:
+                                Console.WriteLine("Команда нераспознана");
+                                break;
+                        }
+                        
+                        
+                        
+                        break;
+                    case "D2":
+                        Console.WriteLine("2");
+                        //WorkWithStrings();
+                        break;
+
+                    case "Escape":
+                        break;
+
+                    default:
+                        Console.WriteLine("Такой команды не сущействует. Для вывода списка команд введите 0");
+                        break;
+                }
+            } while (key.Key != ConsoleKey.Escape);
+        }
+        public static void PrintHelpMessageForArray()
+        {
+            Console.WriteLine("0 - Вывод списка команд\n" +
+                              "1 - Cоздать массив\n" +
+                              "2 - Удалить строки, в которых нет цифр\n" +
+                              "Esc - Выход");
+        }
+    }
+}
---
+++ Дфи6/АrrayFuncs.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class ArrayFuncs
+    {
+
+        public static char[,] CreateArray(bool rnd = true)
+        {
+            int row = GetUserInt("Введите количество строк");
+            int columns = GetUserInt("Введите количество cтолбцов");
+            char[,] arr = new char[row, columns];
+            if (rnd)
+            {
+                FillRandom(ref arr);
+            }
+            else
+            {
+                FillManually(ref arr);
+            }
+            return arr;
+        }
+
+        public static string PrintArray(char[,] arr)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    sb.Append(arr[i, j] + " ");
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
+        }
+
+        public static void FillRandom(ref char[,] arr)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = Convert.ToChar(rnd.Next(0, 128));
+                }
+            }
+        }
+
+        public static void FillManually(ref char[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = GetUserChar();
+                }
+            }
+        }
+
+        public static char GetUserChar()
+        {
+            bool isCorrect;
+            char res;
+            do
+            {
+                string input = Console.ReadLine();
+                isCorrect = char.TryParse(input, out res);
+                if (!isCorrect)
+                {
+                    Console.WriteLine("Введенное значение должно быть символом");
+                }
+                
+
+            }
+            while (!isCorrect);
+            return res;
+        }
+
+        public static int GetUserInt(string message)
+        {
+            Console.WriteLine(message);
+            bool isCorrect;
+            int res;
+            do
+            {
+                string input = Console.ReadLine();
+                isCorrect = int.TryParse(input, out res);
+                if (!isCorrect)
+                {
+                    Console.WriteLine("Введенное значение должно быть числом");
+                }
+                else if (res < 1)
+                {
+                    Console.WriteLine("Число должно быть больше 0");
+                }
+                
+            } 
+            while (!isCorrect);
+            return res;
+        }
+    }
+}
